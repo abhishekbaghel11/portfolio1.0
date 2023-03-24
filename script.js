07cc5b40577bda4e2b7b0f839c646235a00671e9
Abhishek Baghel <122452418+abhishekbaghel11@users.noreply.github.com>
2023-03-25 02:11:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active")
     })
 })
+
+
+//Progress bar animation when it is scrolled at here 
+const animation_elements = document.querySelectorAll(".progress-bar");
+
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry)=> {
+        if(entry.isIntersecting){
+            entry.target.classList.add("animate");
+        }
+        else{
+            entry.target.classList.remove("animate");
+        }
+    })
+}, {
+    threshold: 0.5
+});
+
+for(let i = 0;i<animation_elements.length; i++){
+    const el = animation_elements[i];
+    observer.observe(el);
+}
+
+//Skill boxes appear
+const box_appearances = document.querySelectorAll(".skill .skill-content .box");
+
+const observerSkill = new IntersectionObserver((entries)=> {
+    entries.forEach((entry)=> {
+        if(entry.isIntersecting){
+            entry.target.classList.add("box-appear");
+        }
+        else{
+            entry.target.classList.remove("box-appear");
+        }
+    })
+}, {
+    threshold: 0.5
+});
+
+for(let i = 0;i<box_appearances.length; i++){
+    const el = box_appearances[i];
+    observerSkill.observe(el);
+}
